c795362faefa201ec6c5db72a42c521b71dca7e1
Michael Elsdoerfer <michael@elsdoerfer.info>
2008-07-15 21:55:48 +0200

    added basic 'models' command to display the defined models and their fields

--- feedplatform/management/commands/models.py
+++ feedplatform/management/commands/models.py
@@ -1,5 +1,20 @@
 from feedplatform.management import BaseCommand
+from feedplatform import db
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        pass
+        for model in db.models.values():
+
+            print model.__name__
+
+            # ``_storm_columns`` seems to be the only place we can get
+            # access to to the originally defined ``Property`` objects
+            # (instead of the wrapped ``PropertyColumn``), and thus to
+            # the field type (= the class name).
+            # Fortunately, it is also seems to be the best solution,
+            # as opposed to say using ``dir(model)`` to get the fields.
+            for column, property in model._storm_columns.iteritems():
+                print "    %(name)s: %(type)s" % {
+                    'name': property.name,
+                    'type': column.__class__.__name__
+                }
