f03e70fa3a61856acf0c5ed4b8645d89d060dcea
Michael Elsdoerfer <michael@elsdoerfer.info>
2008-07-24 15:19:35 +0200

    removed some of the unnecessary complexity we just added to the logging module; turms out we don't need most of this stuff

--- feedplatform/log.py
+++ feedplatform/log.py
@@ -5,39 +5,28 @@ within FeedPlatform.
 import sys
 import logging
 
-__all__ = ('log', 'set_logger', 'reset')
+__all__ = ('log', 'reset')
 
 
-_logger = None
-
-# using a proxy allows switching the logger instance even for
-# code that has already imported an older instance.
-class logger_proxy(object):
-    def __getattribute__(self, name):
-        return getattr(_logger, name)
-log = logger_proxy()
-
-def set_logger(l):
-    """Fully replace the logger. This works well only a proxy
-    object is exposed.
-    """
-    global _logger
-    _logger = l
+log = None
 
 def reset():
     """(Re)configure the default logger."""
 
-    log = logging.getLogger('feedplatform')
+    new_logger = logging.getLogger('feedplatform')
 
     # might be an existing object, reset it
-    for handler in log.handlers:
-        log.removeHandler(handler)
-    for filter in log.filters:
-        log.removeFilter(filter)
+    for handler in new_logger.handlers:
+        new_logger.removeHandler(handler)
+    for filter in new_logger.filters:
+        new_logger.removeFilter(filter)
 
-    # configure & use
-    log.addHandler(logging.StreamHandler(sys.stdout))
-    log.level = logging.INFO
-    set_logger(log)
+    # configure
+    new_logger.addHandler(logging.StreamHandler(sys.stdout))
+    new_logger.level = logging.INFO
+
+    # use
+    global log
+    log = new_logger
 
 reset()
