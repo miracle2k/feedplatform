ecaa32b17bc70c37af332abca2eb3d5cfeeaf337
Michael Elsdoerfer <michael@elsdoerfer.info>
2008-07-28 02:14:50 +0200

    prepare for integration of a patched feedparser version

---
+++ feedplatform/deps/feedparser/__init__.py
@@ -0,0 +1,7 @@
+"""Provides a patched version of Universal Feed Parser.
+
+See the **patches/feedparser** directory for more information.
+"""
+
+# TODO: provide an option to use a custom version of feedparser
+from _feedparser import *
