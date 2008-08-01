dc6e5fd5f2ef0b6aa54385aa4e4de24368b13579
Michael Elsdoerfer <michael@elsdoerfer.info>
2008-08-01 20:30:52 +0200

    added a test for the most recent feedparser patch (keep relative guids)

---
+++ tests/feedparser/__init__.py
@@ -0,0 +1,5 @@
+"""Test certain behaviour that we particuarily expect from the 
+feedparser lib more. 
+
+For the most part revolves around custom patches. 
+"""
---
+++ tests/feedparser/test_107_keep-relative_guids.py
@@ -0,0 +1,22 @@
+"""When a guid has isPermaLink="false", it is not forced into an 
+absolute URL.
+"""
+
+from tests import feedev
+from feedplatform import db
+
+class TestFeed(feedev.Feed):
+    url = u"http://base.com/feed"
+    content = """
+    <rss><channel>
+        <item><guid isPermaLink="false">xyz</guid></item>
+        <item><guid>xyz</guid></item>
+    </channel></rss>
+    """
+
+    def pass1(feed):
+        assert feed.items.find(db.Item.guid == u'xyz').count() == 1
+        assert feed.items.find(db.Item.guid == u'http://base.com/xyz').count() == 1        
+
+def test():
+    feedev.testmod()
