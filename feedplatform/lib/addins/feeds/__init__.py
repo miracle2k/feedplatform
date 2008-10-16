d4908b537709ae416692a565c5f295db0d52fed6
Michael Elsdoerfer <michael@elsdoerfer.info>
2008-10-16 14:55:35 +0200

    moved a couple of ideas for future addins to the readme file

--- feedplatform/lib/addins/feeds/__init__.py
+++ feedplatform/lib/addins/feeds/__init__.py
@@ -4,19 +4,3 @@
 from http import *
 from collect_feed_data import *
 from images import *
-
-# TODO:
-def save_bandwith():
-    raise NotImplementedError()
-
-def download_cover():
-    raise NotImplementedError()
-
-def download_favicon():
-    raise NotImplementedError()
-
-def prioritize_parse():
-    raise NotImplementedError()
-
-def respect_ttl():
-    raise NotImplementedError()
