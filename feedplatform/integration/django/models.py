946a1a1530a966ddee562295e906ff734db8f168
Michael Elsdoerfer <michael@elsdoerfer.info>
2008-12-07 12:33:35 +0100

    updated Django DSN maker, added tests

--- feedplatform/integration/django/models.py
+++ feedplatform/integration/django/models.py
@@ -1,12 +1,13 @@
-from django.conf import settings
+from django.conf import settings as django_settings
 
 
-def make_dsn():
+def make_dsn(settings=django_settings):
     """Returns a database connection string suitable for use with
-    FeedPlatform, based on the database configuration of the Django
-    project.
+    FeedPlatform, based on the database configuration of the current
+    Django project.
 
-    # XXX: add tests for this
+    Alternatively, an explit ``settings`` object/module may be
+    specified.
     """
 
     try:
@@ -17,27 +18,25 @@ def make_dsn():
             'sqlite3': 'sqlite',
         }[settings.DATABASE_ENGINE]
     except:
-        raise ValueError('database engine "%s" not supported' %
-                            settings.DATABASE_ENGINE)
+        dsn = settings.DATABASE_ENGINE
     dsn += '://'
 
     if settings.DATABASE_USER:
         dsn += settings.DATABASE_USER
+    dsn += ':'
 
-        if settings.DATABASE_PASSWORD:
-            dsn += ':' + settings.DATABASE_PASSWORD
+    if settings.DATABASE_PASSWORD:
+        dsn += settings.DATABASE_PASSWORD
+    dsn += '@'
 
     if settings.DATABASE_HOST:
-        if settings.DATABASE_USER or settings.DATABASE_PASSWORD:
-            dsn += '@'
         dsn += settings.DATABASE_HOST
+    dsn += ':'
 
-        if settings.DATABASE_PORT:
-            dsn += ':%s' % DATABASE_PORT
+    if settings.DATABASE_PORT:
+        dsn += '%s' % settings.DATABASE_PORT
 
     if settings.DATABASE_NAME:
-        if settings.DATABASE_HOST or settings.DATABASE_PORT:
-            dsn += '/'
-        dsn += settings.DATABASE_NAME
+        dsn += '/' + settings.DATABASE_NAME
 
     return dsn
--- tests/integration/django/test_django.py
+++ tests/integration/django/test_django.py
@@ -1,19 +1,27 @@
+from nose.plugins.skip import SkipTest
+
 import os, sys
+import copy
 import subprocess
 from StringIO import StringIO
 
+from storm.uri import URI
 from feedplatform.management import call_command
 from feedplatform import conf
 
-def test_django_integration():
-    # Check that the output of a manual "call_command" matches what
-    # we get from executing ./manage.py.
 
-    # if django is not installed, don't bother with this test
+def setup_module():
+    # if django is not installed, don't bother with these tests
     try:
         import django
     except ImportError:
-        return
+        raise SkipTest()
+
+
+def test_django_integration():
+    """Check that the output of a manual "call_command" matches what
+    we get from executing ./manage.py.
+    """
 
     # 1. get via process
     managepy = os.path.join(os.path.dirname(__file__), 'proj', 'manage.py')
@@ -50,3 +58,48 @@ def test_django_integration():
     print "command: (%s)" % repr(command_output), len(command_output)
     print "process: (%s)" % repr(process_output), len(process_output)
     assert process_output ==  command_output
+
+
+def test_make_dsn():
+    """Test the Django DSN string builder."""
+
+    # Setup django in the current thread; other tests just
+    # used it via an external process.
+    from django.core.management import setup_environ
+    import proj.settings
+    setup_environ(proj.settings)
+    from feedplatform.integration.django.models import make_dsn
+
+    # Automatically generate a list of all possible variations
+    # of missing connection data. Test both with None and ''
+    # values.
+    base = ['michael', 'xyz123', 'localhost', '3600', 'foo']
+
+    def yield_variants(value, collector, pos=0, blackout=None):
+        collector.append(value)
+        for i in range(pos, len(value)):
+            v = copy.copy(value)
+            v[i] = blackout
+            yield_variants(v, collector, i+1, blackout)
+
+    variants_to_test = []
+    yield_variants(base, variants_to_test, blackout=None)
+    yield_variants(base, variants_to_test, blackout='')
+
+    # Check that the decoded version of what we encode matches the
+    # original, for all possible variations.
+    for user, password, host, port, database in variants_to_test:
+        obj = type('FakeSettings', (), {})()
+        obj.DATABASE_ENGINE = 'mysql'
+        obj.DATABASE_USER = user
+        obj.DATABASE_PASSWORD = password
+        obj.DATABASE_HOST = host
+        obj.DATABASE_PORT = port
+        obj.DATABASE_NAME = database
+        uri = URI(make_dsn(obj))
+
+        for a, b in zip(
+            (uri.username, uri.password, uri.host, uri.port, uri.database),
+            (user, password, host, port, database)
+        ):
+            assert str(a) == str(b) or (a in (None, '') and b in (None, ''))
