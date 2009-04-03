ad93cf5db7344bab5434017c20c2a7e2f9d53d1a
Michael Elsdoerfer <michael@elsdoerfer.info>
2009-04-03 15:37:05 +0200

    Added some TODOs related to the recent new functionality.

---
+++ tests/lib/doers/daemons/test_loop_daemon.py
@@ -0,0 +1,21 @@
+
+
+def test_loop_daemon():
+    #daemon = provide_loop_daemon()
+    #daemon.run()
+
+    # TODO: to test this daemon (and others) we need to have a test
+    # database setup; our test framework does this, but forces us into
+    # the corset of writing feed specific tests.
+    # We need to update the test framework to allow this:
+
+    # env = feedev.environment([Feed1, Feed2, ...])
+    # try:
+    #     ... we can do stuff here, like run the daemon,
+    #         and later check the feeds were updated ...
+    # finally:
+    #     env.close()
+    #
+    # (in 2.6): with feedev.environment: ...
+
+    pass
