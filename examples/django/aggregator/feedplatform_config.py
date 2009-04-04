fb6d70e21880b56e9173358c4e79ff2b6fc493dc
Michael Elsdoerfer <michael@elsdoerfer.info>
2009-04-04 18:56:21 +0200

    Added a queue daemon to the Django example.

--- examples/django/aggregator/feedplatform_config.py
+++ examples/django/aggregator/feedplatform_config.py
@@ -3,7 +3,14 @@ from feedplatform.integration.django.models import make_dsn
 
 DATABASE = make_dsn()
 
+from Queue import Queue
+queue = Queue(0)
+
 ADDINS = [
-    provide_loop_daemon,
+    provide_loop_daemon(name="loop"),
+    provide_multi_daemon(daemons=[
+	provide_queue_daemon(queue),
+	provide_socket_queue_controller(queue, ('localhost', 7777))
+    ], name="queue"),
     collect_feed_data('title', 'updated', 'summary', 'language'),
 ]
